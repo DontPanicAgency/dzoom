b3b9337589825eeb273e3f1651abef5705fb323f
Stuart Lawrence <stuart@dontpanicdesign.co.uk>
2020-04-02 15:34:14 +0100

    Added counter

--- js/app.js
+++ js/app.js
@@ -12,3 +12,26 @@ $buttons.forEach($el => {
     return false;
   });
 });
+
+var $counter = document.querySelector('.counter_object');
+var counterValue = 10000;
+function formatNumber(num) {
+  return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+}
+
+function increase_counter() {
+
+  counterValue+=152689;
+
+  if(counterValue > 350000000)
+    counterValue = 350000000;
+
+  let count_str = formatNumber(counterValue);
+  $counter.innerHTML = '<span class="d">' + count_str.replace(/,/gi, '</span><span class="d">') + '</span>';
+
+  if(counterValue < 350000000) {
+    window.requestAnimationFrame(increase_counter);
+  }
+}
+
+increase_counter();
